8662328fbcb26cbbe26197d708c4835c324b135c
tarjoilija <admin@awly.org>
2013-09-30 18:02:32 +0300

    Modify image.lua

--- sys/lua/lapi/core/image.lua
+++ sys/lua/lapi/core/image.lua
@@ -4,18 +4,8 @@ Image = {}
 Image.mt = {}
 
 setmetatable(Image, {
-    __call = function(_, arg1, ...)
-        if type(arg1) == "number" then
-            return setmetatable({id = arg1}, Image.mt)
-        elseif type(arg1) == "table" then
-            local tbl = {}
-            for k,v in pairs(arg1) do
-                table.insert(tbl, Image(v))
-            end
-            return tbl
-        else
-            return Image(image(arg1, unpack({...})))
-        end
+    __call = function(_, path, x, y, mode, ...)
+        return setmetatable({id = image(path, x, y, mode, unpack({...}))}, Image.mt)
     end,
     __index = function(_, key)
         local m = rawget(Image, key)

0a17274d1dbab90e7f2976a3e6ecaa08c5fda148
Terracraft <jonirovio10@gmail.com>
2017-12-07 17:44:13 +0100

    Update methods.py

--- sys/lua/ttt/methods.py
+++ sys/lua/ttt/methods.py
@@ -1,12 +1,10 @@
 import glob
-
 storage = ''
-
 for file_name in glob.glob('./*.lua'):
     with open(file_name, 'r') as f:
         storage += file_name + ':\n'
         for line in f:
-            if 'function ' in line:
+            if 'function ' in line: 
                 storage += line[9:]                
         storage += '\n'
 print storage
